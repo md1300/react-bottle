cea0f0a7cb28a6261fab73814ecca1a51b7e9a1c
md1300 <md.tawhid.khan1998@gmail.com>
2024-05-30 02:08:00 +0600

    second commit

---
+++ src/utility/localStorage.js
@@ -0,0 +1,18 @@
+const getStoreCart=()=>{
+    const storeCartString=localStorage.getItem('cart')
+    if(storeCartString){
+        return JSON.parse(storeCartString)
+    }
+    return []
+}
+const saveCartToLS=(cart)=>{
+    const cartStringified=JSON.stringify(cart)
+    localStorage.setItem('cart',cartStringified)
+}
+
+const addToLS=id=>{
+    const cart=getStoreCart();
+    cart.push(id)
+    //------------------- save to local storage  --------------
+    saveCartToLS()
+}
